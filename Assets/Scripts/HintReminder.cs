631b8599e768a3eb84187f9344366889a8612a62
BobbyTheDude <bobbyvivian0@gmail.com>
2020-08-05 19:23:27 -0700

    dark mode complete, hint system fixed

---
+++ Assets/Scripts/HintReminder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintReminder : MonoBehaviour
+{
+    public GameObject HintText;
+    public GameObject PauseCanvas;
+    void Start()
+    {
+        StartCoroutine(StartShowing());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            PauseCanvas.GetComponent<PauseMenu>().HintButton();
+            HintText.SetActive(false);
+        }
+    }
+    IEnumerator StartShowing()
+    {
+        yield return new WaitForSeconds(30);
+        HintText.SetActive(true);
+        StartCoroutine(StopShowing());
+    }
+    IEnumerator StopShowing()
+    {
+        yield return new WaitForSeconds(10);
+        HintText.SetActive(false);
+    }
+}
